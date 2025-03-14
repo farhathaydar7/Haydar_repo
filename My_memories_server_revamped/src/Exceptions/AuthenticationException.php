5f1e6ef16ec13a1a395e7a75c6c26cf1e32cec9d
farhathaydar7 <haydarfarhatsteam69@gmail.com>
2025-03-14 11:30:06 +0200

    Succsefully made a login endpoint

--- My_memories_server_revamped/src/Exceptions/AuthenticationException.php
+++ My_memories_server_revamped/src/Exceptions/AuthenticationException.php
@@ -1,15 +1,8 @@
 <?php
 namespace MyApp\Exceptions;
 
-class ValidationException extends \InvalidArgumentException {
-    public function __construct($message = "Invalid input", $code = 400) {
-        parent::__construct($message, $code);
-    }
-}
-
 class AuthenticationException extends \RuntimeException {
     public function __construct($message = "Authentication failed", $code = 401) {
         parent::__construct($message, $code);
     }
 }
-?>
